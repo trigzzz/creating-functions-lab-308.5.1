0008c3c7942f5209d32dbc0490321d42abc4bed4
Kevin Vargas <TriGzYT@gmail.com>
2023-11-30 16:17:15 -0500

    part 1 of step 1 done

---
+++ index.js
@@ -0,0 +1,26 @@
+// Part 1: Thinking Functionally
+
+// Take an array of numbers and return the sum.Take an array of numbers and return the sum.
+    function calculateSum(myNums) {
+        let sum = 0;
+      
+        for (let i = 0; i < myNums.length; i++) {
+          sum += myNums[i];
+        }
+      
+        return sum;
+      }
+    
+    
+    const myArray = [1, 2, 3, 4, 5];
+    const result = calculateSum(myArray);
+    console.log(result);
+
+    // Take an array of numbers and return the average.
+    
+
+
+
+
+    //Part 2: Thinking Methodically
+
