1305b77079e70557428cf250387fdfefcdf4e682
Kevin Vargas <TriGzYT@gmail.com>
2023-12-03 22:24:01 -0500

    completed part 1

--- index.js
+++ index.js
@@ -35,6 +35,42 @@
 
     // Take an array of strings and return the longest string.
 
+    function longestString(strings) {
+        if (strings.length === 0) {
+            return null; 
+        }
+    
+        return strings.reduce((longest, current) => (current.length > longest.length ? current : longest), strings[0]);
+    }
+    
+    const strings = ['say', 'hello', 'in', 'the', 'morning'];
+    const resultLongestString = longestString(strings);
+    console.log(resultLongestString); 
+    
+    // Take an array of strings, and a number and return an array of the strings that are longer than the given number.
+    
+    function stringsLongerThan(arr, length) {
+        return arr.filter(str => str.length > length);
+    }
+    
+    const inputArray = ['im', 'loving', 'java', 'script', 'woohoo!'];
+    const minLength = 4;
+    
+    const resultArray = stringsLongerThan(inputArray, minLength);
+    console.log(resultArray);
+
+    //Take a number, n, and print every number between 1 and n without using loops. Use recursion.
+
+    function printNumbersRecursive(n) {
+        if (n > 0) {
+            printNumbersRecursive(n - 1);
+            console.log(n);
+        }
+    }
+    
+    const n = 8;
+    printNumbersRecursive(n);
+    
     
 
 
